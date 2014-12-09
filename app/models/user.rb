de428cfe1b62f7193edf6d0e4542f54b82e621ad
JasonGuanGitHub <guanjichang1226@163.com>
2014-12-09 19:37:54 +0800

    重构welcome controller 将controller里的业务逻辑移到model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,28 @@ class User < ActiveRecord::Base
   has_many :appointments
   has_many :messages
   has_and_belongs_to_many :groups
+  
+  def register(register)
+    self.name = register[:name]
+    self.email = register[:email]
+    self.password = register[:password]
+    self.role_id = 1
+    self.save
+    return true
+  end
+
+  def validates(login)
+    @user = User.where( email: login[:email] ).take
+    if @user ==nil
+    return false
+    else if @user.password == login[:password]
+      self.id = @user.id
+      self.name = @user.name
+      return true
+      else
+      return false
+      end
+    end
+  end
+
 end
