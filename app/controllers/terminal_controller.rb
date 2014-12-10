50c0ba4ecd4166a04e84a7a9915152bae8ee2d82
JasonGuanGitHub <guanjichang1226@163.com>
2014-12-10 21:51:33 +0800

    修正终端预约列表显示

--- app/controllers/terminal_controller.rb
+++ app/controllers/terminal_controller.rb
@@ -1,25 +1,21 @@
 class TerminalController < ApplicationController
   layout 'customerlayout'
   def login
-    
+
   end
-  
+
   def index
-  # @appointments = User.find(session[:user_id]).appointments
-  
-    @appointments = Instrument.first.appointments.where("date<=?",format_todate).order(date: :asc, time: :asc)
-
-    #@appointments=@appointments.where()
+    # @appointments = User.find(session[:user_id]).appointments
+    @appointment = Appointment.new
+    
+    @appointments = @appointment.get_appointments_by_instrument_id(1)
 
   end
-  
-  
-  
-  
+
   def validates
     login=params[:login]
     @user=User.where( email: login[:email] ).take
-    
+
     if @user.email != ''
       session[:user_id]=@user.id
       session[:user_name]=@user.name
@@ -28,27 +24,5 @@ class TerminalController < ApplicationController
       redirect_to terminal_login_url
     end
   end
-  
-  
-  private 
-  
-  def format_todate
-     #Date date=new Date()
-  #  SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd")
-    # String time=df.format(date)
-    
-    time = Time.now
-    time.strftime("YYYY-MM-DD")
-    
-end
 
-def format_totime
-     #Date date=new Date()
-  #  SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd")
-    # String time=df.format(date)
-    
-    time = Time.now
-    time.strftime("hh:mm")
-    
-end
 end
